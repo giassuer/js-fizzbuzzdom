d97bdbeaab4259fd18b511260c2fd0242724952c
giassuer <g.assuero@hotmail.it>
2021-11-16 14:53:42 +0100

    milestone-3

---
+++ milestone-3/js/script.js
@@ -0,0 +1,38 @@
+const boxesContainer = document.querySelector('.container');
+
+
+for(let i = 0; i <= 100; i++){
+
+    let fizzBuzz;
+    let newBox;
+
+    if( i % 5 === 0 && i % 3 === 0 ){
+        fizzBuzz = 'fizzbuzz';
+        newBox = `
+        <div class="box fizzbuz">${fizzBuzz}</div>
+    `;
+        console.log(fizzBuzz)
+
+    }else if( i % 3 === 0 ){
+        fizzBuzz = 'fizz';
+        newBox = `
+        <div class="box fizz">${fizzBuzz}</div>
+    `;
+        console.log(fizzBuzz)
+
+    }else if( i % 5 === 0 ){
+        fizzBuzz = 'buzz';
+        newBox = `
+        <div class="box buzz">${fizzBuzz}</div>
+    `;
+        console.log(fizzBuzz)
+
+    }else{
+        newBox = `
+        <div class="box number">${i}</div>
+    `;
+        console.log(i)
+    }
+    
+    boxesContainer.innerHTML += newBox;
+}
